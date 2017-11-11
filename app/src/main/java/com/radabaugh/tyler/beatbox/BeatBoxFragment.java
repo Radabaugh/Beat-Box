d7a8efec7b9c14eea4e53dc9ba09770e32f6bb1c
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-11-10 16:54:06 -0800

    Chap 20 pg 376, Creating BeatBox

---
+++ app/src/main/java/com/radabaugh/tyler/beatbox/BeatBoxFragment.java
@@ -0,0 +1,13 @@
+package com.radabaugh.tyler.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tyler Radabaugh on 11/10/2017.
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
